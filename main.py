a131326776719ffba3faeafc3b9652a8b92829bc
shubhranshu-sahu <shubhranshu2023@gmail.com>
2025-07-25 08:20:43 +0530

    Change app.py name , and modified it

---
+++ main.py
@@ -0,0 +1,14 @@
+from app import create_app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    import os
+    import logging
+    logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+    debug_mode = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    app.run(debug=debug_mode)
